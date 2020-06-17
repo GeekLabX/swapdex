75d4904b22adc34e999294f34f86581af0a9932b
waylandc <wayland.chan@gmail.com>
2020-06-17 16:31:02 +0800

    convert to typescript

--- parrot-client/.eslintrc.js
+++ parrot-client/.eslintrc.js
@@ -1,23 +1,15 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es6: true,
-    },
-    extends: [
-        'airbnb-base',
-    ],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-        ecmaVersion: 2018,
-    },
-    rules: {
-
-        "indent": ["error", 4]
-
-
-    },
+	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+	extends: [
+		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+	],
+	parserOptions: {
+		ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+		sourceType: 'module', // Allows for the use of imports
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'no-console': 0,
+		'no-tabs': 0,
+	},
 };
